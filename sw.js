b2cdf43c8dd9832fb5c38bc0f4ab2af9b9f1cf13
TendoKou <263867953+TendoKou@users.noreply.github.com>
2026-03-16 23:33:57 +0900

    sw.js を更新

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ self.addEventListener('message', (event) => {
   }
 });
 
-const CACHE_NAME = 'my-game-center-v1';
+const CACHE_NAME = 'my-game-center-v1.1';
 const urlsToCache = [
   './',
   './index.html',
